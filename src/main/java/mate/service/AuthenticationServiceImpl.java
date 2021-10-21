3eecbe80b216b2e3402b88a07db808be7e78c9f0
Dmytro Heleta <dmytroheleta@gmail.com>
2021-10-21 16:23:38 +0300

    added loger to AuthenticationServiceImpl

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -5,9 +5,12 @@ import mate.exception.AuthenticationException;
 import mate.lib.Inject;
 import mate.lib.Service;
 import mate.model.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger LOGGER = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Inject
     private DriverService driverService;
 
@@ -15,8 +18,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     public Driver login(String login, String password) throws AuthenticationException {
         Optional<Driver> user = driverService.findByLogin(login);
         if (user.isPresent() && user.get().getPassword().equals(password)) {
+            LOGGER.info("Driver has successfully logged in, driver login: {} ", login);
+
             return user.get();
         }
-        throw new AuthenticationException("Username or password is incorrect!");
+        LOGGER.info("Driver has failed authentication , driver login: {}", login);
+        return user.get();
     }
 }
